f2de429a54c6e91228a85e1a723f0c59ce4961cf
Andrew <kajij@users.noreply.github.com>
2019-10-11 21:03:53 +0200

    Added new RotatingLissajousFigure class that extends base class

--- LissajousRotating/main.js
+++ LissajousRotating/main.js
@@ -1,4 +1,4 @@
-import Lissajous from "../Lissajous/LissajousFigure.js";
+import RotatingLissajousFigure from "./RotatingLissajousFigure.js";
 import * as helpers from "../Utils/helpers.js";
 import Vector2D from "../Utils/Vector2D.js";
 
@@ -21,7 +21,7 @@ var fgCtx = foregroundCanvas.getContext('2d');
 
 let center = new Vector2D(Math.floor(canvasWidth/2), Math.floor(canvasHeight/2));
 
-var lissajous = new Lissajous(center, 400, 3, 8, 0, Math.PI/2);
+var lissajous = new RotatingLissajousFigure(center, 400, 3, 8, 0, Math.PI/2);
 
 let t = helpers.range(0, 13, 0.01);
 let i = 0;
