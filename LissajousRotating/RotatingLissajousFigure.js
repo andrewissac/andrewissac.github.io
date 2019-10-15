c1f53f6929e5daaddb8abcc0d225a6c9d0bf9d1d
Andrew <kajij@users.noreply.github.com>
2019-10-15 23:01:53 +0200

    finished lissajousortating

--- LissajousRotating/RotatingLissajousFigure.js
+++ LissajousRotating/RotatingLissajousFigure.js
@@ -18,21 +18,21 @@ export default class RotatingLissajousFigure extends Lissajous {
 
 	DrawWholeFigure(bgContext, fgContext) {
 		const t = helpers.range(0, 6.28, 0.01);
-		fgContext.beginPath();
-		fgContext.save();
+		bgContext.beginPath();
+		bgContext.save();
 		const hue = this.phaseshift2 * 100 > 255 ? this._phaseshift2 * 100 - 255 : this.phaseshift2 * 100; // makes sure rainbowcolors are repeating
-		fgContext.strokeStyle = "hsl(" + hue + ", 100%,  78%)";
-		fgContext.lineWidth = 2;
+		bgContext.strokeStyle = "hsl(" + hue + ", 100%,  78%)";
+		bgContext.lineWidth = 2;
 		let pos = new Vector2D(0, 0);
 		let newPos = new Vector2D(0, 0);
 
 		for (let i = 0; i < t.length - 1; i++) {
 			pos = this.center.Add(this.CalcXY(t[i]));
 			newPos = this.center.Add(this.CalcXY(t[i + 1]));
-			fgContext.moveTo(pos.x, pos.y);
-			fgContext.lineTo(newPos.x, newPos.y);
+			bgContext.moveTo(pos.x, pos.y);
+			bgContext.lineTo(newPos.x, newPos.y);
 		}
-		fgContext.stroke();
-		fgContext.restore();
+		bgContext.stroke();
+		bgContext.restore();
 	}
 }
